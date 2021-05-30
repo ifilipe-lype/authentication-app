388e94de745670e489aa31a76cbaaec64fa20803
ifilipe.lype <filipe7lype@gmail.com>
2021-05-30 12:52:04 +0100

    adds custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Noto Sans', ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         gray: {
-          4: "#828282",
+          1: "#E0E0E0",
+          3: "#828282",
+          4: "#4F4F4F",
           5: "#BDBDBD",
           6: "#333333",
+          dark: "#252329",
         },
         blue: {
           1: "#2F80ED",
