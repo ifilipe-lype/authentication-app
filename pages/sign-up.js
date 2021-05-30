12905f241bc460694a5d73f5629ebce186f15f6f
ifilipe.lype <filipe7lype@gmail.com>
2021-05-30 12:57:48 +0100

    basic struture

---
+++ pages/sign-up.js
@@ -0,0 +1,11 @@
+export default function SignUp() {
+  return (
+    <div>
+      <main className="flex items-center justify-center h-screen w-full">
+        <h1 className="text-gray-700">
+          Hello, world!
+        </h1>
+      </main>
+    </div>
+  )
+}
