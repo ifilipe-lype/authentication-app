e6d663bf1174bb79cd14b5de215a1d107f271b69
ifilipe.lype <filipe7lype@gmail.com>
2021-05-30 13:11:27 +0100

    done with the header section

--- pages/sign-up.js
+++ pages/sign-up.js
@@ -2,9 +2,19 @@ export default function SignUp() {
   return (
     <div>
       <main className="flex items-center justify-center h-screen w-full">
-        <h1 className="text-gray-700">
-          Hello, world!
-        </h1>
+        <section className="md:max-w-md w-full rounded-lg py-14 px-10 border">
+            <header>
+                <h1>
+                    <img src="/devchallenges.svg" />
+                </h1>
+                <h3 className="text-lg text-gray-6 font-semibold leading-tight mt-8">
+                    Join thousands of learners from around the world
+                </h3>
+                <p className="text-gray-6 mt-3 text-base leading-tight">
+                    Master web development by making real-life projects. There are mutliple paths for you to choose.
+                </p>
+            </header>
+        </section>
       </main>
     </div>
   )
