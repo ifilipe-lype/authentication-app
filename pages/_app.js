141819ebfdedd199fd430a821a1960310212db4a
ifilipe.lype <filipe7lype@gmail.com>
2021-05-30 23:51:47 +0100

    adds support for dark theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,17 @@
+import { ThemeProvider } from 'next-themes'
 import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider attribute="class">
       <Head>
         <meta name="description" content="An authentication app example"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" /> 
       </Head>
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
