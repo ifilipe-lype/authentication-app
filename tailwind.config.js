b3844d5baf9852b55aa053bc4f6edef9e11ae43f
ifilipe.lype <filipe7lype@gmail.com>
2021-05-30 11:59:18 +0100

    adds project colors theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: "media", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          4: "#828282",
+          5: "#BDBDBD",
+          6: "#333333",
+        },
+        blue: {
+          1: "#2F80ED",
+        }
+      }
+    },
   },
   variants: {
     extend: {},
