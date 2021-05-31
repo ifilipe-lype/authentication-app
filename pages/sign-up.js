64000dbf106a977d87541a6dbeac7f2104a170ec
ifilipe.lype <filipe7lype@gmail.com>
2021-05-31 13:23:51 +0100

    adds required prop to inputs

--- pages/sign-up.js
+++ pages/sign-up.js
@@ -52,7 +52,7 @@ export default function SignUp() {
                                 <span>
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#000000"><path d="M0 0h24v24H0z" fill="none" /><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" /></svg>
                                 </span>
-                                <input type="text" placeholder="name" />
+                                <input required type="text" placeholder="name" />
                             </label>
                         </div>
                         <div className="input-group">
@@ -60,7 +60,7 @@ export default function SignUp() {
                                 <span>
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#000000"><path d="M0 0h24v24H0z" fill="none" /><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" /></svg>
                                 </span>
-                                <input type="email" placeholder="email" />
+                                <input required type="email" placeholder="email" />
                             </label>
                         </div>
                         <div className="input-group">
@@ -68,7 +68,7 @@ export default function SignUp() {
                                 <span>
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none" /><path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1v2z" /></svg>
                                 </span>
-                                <input type="password" placeholder="password" />
+                                <input required type="password" placeholder="password" />
                             </label>
                         </div>
 
