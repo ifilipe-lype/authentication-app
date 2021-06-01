6fb629b936119b56a35adff00cbe2813d74ae2d4
ifilipe.lype <filipe7lype@gmail.com>
2021-06-01 15:29:46 +0100

    exports an social icons

---
+++ components/social-logins.js
@@ -0,0 +1,26 @@
+export default function SocialLogins() {
+    return (
+        <ul className="flex items-center mb-3">
+            <li className="mr-5">
+                <button className="outline-none focus:outline-none">
+                    <img src="/Google.svg" />
+                </button>
+            </li>
+            <li className="mr-5">
+                <button className="outline-none focus:outline-none">
+                    <img src="/Facebook.svg" />
+                </button>
+            </li>
+            <li className="mr-5">
+                <button className="outline-none focus:outline-none">
+                    <img src="/Twitter.svg" />
+                </button>
+            </li>
+            <li className="mr-5">
+                <button className="outline-none focus:outline-none">
+                    <img src="/Github.svg" />
+                </button>
+            </li>
+        </ul>
+    )
+}
