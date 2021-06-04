af705ae86f93fae1652ecceb55a2bdbb5b6d9fb0
ifilipe.lype <filipe7lype@gmail.com>
2021-06-04 01:19:01 +0100

    exports hook for protecting routes

---
+++ hooks/useRequireAuth.js
@@ -0,0 +1,17 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useAuth } from "./useAuth.js";
+
+export function useRequireAuth(redirectTo = "/login") {
+  const auth = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    let token = JSON.parse(localStorage.getItem("token"));
+    if (!token) {
+      router.push(redirectTo);
+    }
+  }, [auth, router]);
+
+  return auth;
+}
