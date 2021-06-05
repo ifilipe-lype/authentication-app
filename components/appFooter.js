9758147666f380b254dfd249b57035d88cbb1099
ifilipe.lype <filipe7lype@gmail.com>
2021-06-05 12:20:32 +0100

    exports footer component

---
+++ components/appFooter.js
@@ -0,0 +1,13 @@
+export default function AppFooter() {
+    return (
+        <footer className="mt-6 flex items-center justify-between w-full">
+            <span className="text-sm text-gray-3">
+                by <a className="hover:text-blue-1" href="https://twitter.com/ifilipe_lype">@ifilipe-lype</a>
+            </span>
+
+            <span className="text-sm text-gray-3">
+                <a className="hover:text-gray-4" href="https://devchallenges.io">devChallenges.io</a>
+            </span>
+        </footer>
+    )
+}
