a6a9020c393865aedb2d97a808009cc3115fd3b6
ifilipe.lype <filipe7lype@gmail.com>
2021-06-13 04:44:13 +0100

    done with image uploading feature

--- components/profile-edit-form.js
+++ components/profile-edit-form.js
@@ -1,5 +1,5 @@
 import { useFormik } from "formik";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import * as yup from "yup";
 
 const schema = yup.object().shape({
@@ -11,6 +11,7 @@ const schema = yup.object().shape({
     phone: yup.string(),
     bio: yup.string(),
     photo: yup.string(),
+    imageFile: yup.mixed()
 });
 
 export default function ProfileEditForm({
@@ -19,9 +20,13 @@ export default function ProfileEditForm({
 }) {
 
     const [isSubmting, setIsSubmting] = useState(false);
+    const [thumb, setThumb] = useState(null);
+
+    const [fileInputError, setFileInputError] = useState("");
 
     const formik = useFormik({
         initialValues: {
+            imageFile: null,
             photo: user.photo,
             name: user.name,
             email: user.email,
@@ -31,8 +36,13 @@ export default function ProfileEditForm({
         },
         validationSchema: schema,
         onSubmit: async (values) => {
+            const multipartFormData = new FormData();
+            for (let [key, value] of Object.entries(values)) {
+                multipartFormData.append(key, value);
+            }
+
             setIsSubmting(true);
-            await updateProfile(values);
+            await updateProfile(multipartFormData);
 
             setIsSubmting(false);
         },
@@ -44,30 +54,84 @@ export default function ProfileEditForm({
         errors,
         touched,
         handleChange,
+        setFieldValue,
         handleBlur,
         handleSubmit
     } = formik;
 
+    function clearFileInput(){
+        setFieldValue("imageFile", null);
+        setThumb(null);
+    }
+
+    useEffect(() => {
+        let reader = new FileReader();
+
+        const maxFileSize = 5 * 1024 * 1024;
+        reader.onloadend = () => {
+            const file = values.imageFile;
+            if (file) {
+                if (file.size <= maxFileSize && (/image/.test(file.type))) {
+                    setFileInputError(false);
+                    setThumb(reader.result);
+                } else {
+                    setFieldValue("imageFile", null);
+                    setThumb(null);
+                    setFileInputError("Can only upload image files under 5 MB");
+                }
+            }
+        };
+
+
+
+        values.imageFile && reader.readAsDataURL(values.imageFile);
+    }, [values.imageFile])
+
 
     return (
         <form onSubmit={handleSubmit} className="text-gray-6 md:w-8/12 dark:text-gray-5">
-            {/* <div className="form-group flex flex-row items-center">
-                <label className="relative block w-16 h-16 transition text-gray-5 hover:text-white cursor-pointer rounded overflow-hidden">
-                    <img src={values.photo} className="w-16 h-16 object-cover object-top" alt="user's photo" />
-                    <div className="bg-gray-dark bg-opacity-50 flex items-center justify-center h-full w-full absolute inset-0">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
-                        </svg>
-                    </div>
-                    <input type="file" className="sr-only" />
-                </label>
-                <span className="ml-6 uppercase text-gray-6 text-sm dark:text-gray-5">change photo</span>
-            </div> */}
-            
+            <div className="form-group">
+                <div className="flex flex-row items-center">
+                    <label className="relative w-16 h-16 transition bg-gray-3 flex items-center justify-center text-gray-5 hover:text-white cursor-pointer rounded overflow-hidden">
+                        <img src={thumb} className="w-16 text-center h-16 object-cover object-top" alt="user's photo" />
+                        <div className="bg-gray-dark bg-opacity-50 flex items-center justify-center h-full w-full absolute inset-0">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
+                            </svg>
+                        </div>
+                        <input
+                            type="file"
+                            name="imageFile"
+                            id="imageFile"
+                            value={undefined}
+                            onBlur={handleBlur}
+                            onChange={(e) => {
+                                formik.setFieldValue("imageFile", e.currentTarget.files[0])
+                            }}
+                            className="sr-only" />
+                    </label>
+                    <span className="ml-6 uppercase text-gray-6 text-sm dark:text-gray-5">change photo</span>
+                </div>
+                <small className="text-red-400 mt-1">{fileInputError}</small>
+                <div>
+                    {
+                        thumb && (
+                            <button type="button" onClick={clearFileInput} className="text-red-400 w-auto text-xs bg-red-100 dark:bg-transparent p-1 rounded-lg outline-none focus:outline-none mt-1">
+                                clear selected image
+                            </button>
+                        )
+                    }
+                </div>
+            </div>
+
+
             <div className="form-group">
                 <label className="mb-1">
                     Photo's url
+                </label>
+                <label className="mb-1 text-xs">
+                    image upload will override photo url
                 </label>
                 <input
                     id="photo"
