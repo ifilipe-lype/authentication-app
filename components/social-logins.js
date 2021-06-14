719246e1577da7c5e3bedc2012e7d2bdab8bb156
ifilipe.lype <filipe7lype@gmail.com>
2021-06-14 14:29:48 +0100

    adds click event listener

--- components/social-logins.js
+++ components/social-logins.js
@@ -1,23 +1,28 @@
 export default function SocialLogins() {
+
+    function alertUnderConstructionFeature(){
+        alert("This feature is under development, please be patient!");
+    }
+
     return (
         <ul className="flex items-center mb-3">
             <li className="mr-5">
-                <button className="outline-none focus:outline-none">
+                <button onClick={alertUnderConstructionFeature} className="outline-none focus:outline-none">
                     <img src="/Google.svg" />
                 </button>
             </li>
             <li className="mr-5">
-                <button className="outline-none focus:outline-none">
+                <button onClick={alertUnderConstructionFeature} className="outline-none focus:outline-none">
                     <img src="/Facebook.svg" />
                 </button>
             </li>
             <li className="mr-5">
-                <button className="outline-none focus:outline-none">
+                <button onClick={alertUnderConstructionFeature} className="outline-none focus:outline-none">
                     <img src="/Twitter.svg" />
                 </button>
             </li>
             <li className="mr-5">
-                <button className="outline-none focus:outline-none">
+                <button onClick={alertUnderConstructionFeature} className="outline-none focus:outline-none">
                     <img src="/Github.svg" />
                 </button>
             </li>
