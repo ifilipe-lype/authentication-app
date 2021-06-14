d5c9130cf9a7bf26a58d0e2ca584ad9441749362
ifilipe.lype <filipe7lype@gmail.com>
2021-06-14 13:14:34 +0100

    fixes broken imports

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Link from "next/link";
-import { useRequireAuth } from "../../hooks/useRequireAuth";
+import { useRequireAuth } from "../../hooks/useAuth";
 import LoadingPage from "../../components/loading-page";
 
 import ProfileDropDownMenu from "../../components/profileDropdownMenu";
